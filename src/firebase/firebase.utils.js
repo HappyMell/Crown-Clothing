4883b109bf62c755981f0c1f2939c5ef7bdebc68
Mellisa Madrid <mellisamadrid@hotmail.com>
2020-07-16 12:35:39 +0100

    integrated firebase, made signin auth

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBryGrXEe51hwDDB0_YQNhPBLm04S8ZEb0",
+  authDomain: "crown-db-cf562.firebaseapp.com",
+  databaseURL: "https://crown-db-cf562.firebaseio.com",
+  projectId: "crown-db-cf562",
+  storageBucket: "crown-db-cf562.appspot.com",
+  messagingSenderId: "444672241210",
+  appId: "1:444672241210:web:0a49374a5b06d24fb94f03",
+  measurementId: "G-GPXD8KNJ8M",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
