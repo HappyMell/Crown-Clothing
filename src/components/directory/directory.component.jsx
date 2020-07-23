31aedd5ab9e883dd7c4116a916c292bc36cd9e19
Mellisa Madrid <mellisamadrid@hotmail.com>
2020-07-23 17:18:18 +0100

    bug fixes

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,13 +6,15 @@ import "./directory.styles.scss";
 
 import MenuItem from "../menu-item/menu-item.component";
 
-const Directory = ({ sections }) => (
-  <div className='directory-menu'>
-    {sections.map(({ id, ...otherSectionProps }) => (
-      <MenuItem key={id} {...otherSectionProps} />
-    ))}
-  </div>
-);
+const Directory = ({ sections }) => {
+  return (
+    <div className='directory-menu'>
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   sections: selectDirectorySections,
