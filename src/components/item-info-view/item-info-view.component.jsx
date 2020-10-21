18c523208176d803fce5774b430fcc884a18bebc
Mellisa Madrid <mellisamadrid@hotmail.com>
2020-10-22 00:10:45 +0100

    node modules change

--- src/components/item-info-view/item-info-view.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./item-info-view.styles.scss";
-
-const ItemInfoView = ({ item }) => {
-  //console.log(item);
-  return <div>View</div>;
-};
-
-export default ItemInfoView;
